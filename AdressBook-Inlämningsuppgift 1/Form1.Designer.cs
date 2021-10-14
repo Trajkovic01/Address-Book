39d418fd4cb5641c891f5e3c5a813a2fc655ac72
Aleksandar Trajkovic <Aleksandar.trajkovic01@gmail.com>
2021-10-14 21:20:58 +0200

    init project

---
+++ AdressBook-Inlämningsuppgift 1/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿
+namespace AdressBook_Inlämningsuppgift_1
+{
+    partial class FormOne
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxCity = new System.Windows.Forms.TextBox();
+            this.textBoxPostNumber = new System.Windows.Forms.TextBox();
+            this.textBoxAdress = new System.Windows.Forms.TextBox();
+            this.textBoxLastname = new System.Windows.Forms.TextBox();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.textBoxMail = new System.Windows.Forms.TextBox();
+            this.listBoxOne = new System.Windows.Forms.ListBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.textBoxFind = new System.Windows.Forms.TextBox();
+            this.listBoxTwo = new System.Windows.Forms.ListBox();
+            this.buttonShowInfo = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lastname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Adress";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 218);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Post Number";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 267);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Post Office";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 314);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Number";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 362);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 15);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Mail";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(105, 77);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 23);
+            this.textBoxName.TabIndex = 7;
+            // 
+            // textBoxCity
+            // 
+            this.textBoxCity.Location = new System.Drawing.Point(105, 259);
+            this.textBoxCity.Name = "textBoxCity";
+            this.textBoxCity.Size = new System.Drawing.Size(100, 23);
+            this.textBoxCity.TabIndex = 8;
+            // 
+            // textBoxPostNumber
+            // 
+            this.textBoxPostNumber.Location = new System.Drawing.Point(105, 210);
+            this.textBoxPostNumber.Name = "textBoxPostNumber";
+            this.textBoxPostNumber.Size = new System.Drawing.Size(100, 23);
+            this.textBoxPostNumber.TabIndex = 9;
+            // 
+            // textBoxAdress
+            // 
+            this.textBoxAdress.Location = new System.Drawing.Point(105, 167);
+            this.textBoxAdress.Name = "textBoxAdress";
+            this.textBoxAdress.Size = new System.Drawing.Size(100, 23);
+            this.textBoxAdress.TabIndex = 10;
+            // 
+            // textBoxLastname
+            // 
+            this.textBoxLastname.Location = new System.Drawing.Point(105, 123);
+            this.textBoxLastname.Name = "textBoxLastname";
+            this.textBoxLastname.Size = new System.Drawing.Size(100, 23);
+            this.textBoxLastname.TabIndex = 11;
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.Location = new System.Drawing.Point(105, 306);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(100, 23);
+            this.textBoxNumber.TabIndex = 12;
+            // 
+            // textBoxMail
+            // 
+            this.textBoxMail.Location = new System.Drawing.Point(105, 354);
+            this.textBoxMail.Name = "textBoxMail";
+            this.textBoxMail.Size = new System.Drawing.Size(100, 23);
+            this.textBoxMail.TabIndex = 13;
+            // 
+            // listBoxOne
+            // 
+            this.listBoxOne.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxOne.FormattingEnabled = true;
+            this.listBoxOne.ItemHeight = 15;
+            this.listBoxOne.Location = new System.Drawing.Point(266, 77);
+            this.listBoxOne.Name = "listBoxOne";
+            this.listBoxOne.Size = new System.Drawing.Size(508, 184);
+            this.listBoxOne.TabIndex = 14;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(266, 285);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 17;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(352, 285);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 18;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(442, 285);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(93, 23);
+            this.buttonUpdate.TabIndex = 19;
+            this.buttonUpdate.Text = "Update adress";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(663, 259);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(113, 23);
+            this.buttonOpen.TabIndex = 20;
+            this.buttonOpen.Text = "Show all addresses";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // textBoxFind
+            // 
+            this.textBoxFind.Location = new System.Drawing.Point(352, 490);
+            this.textBoxFind.Multiline = true;
+            this.textBoxFind.Name = "textBoxFind";
+            this.textBoxFind.Size = new System.Drawing.Size(168, 25);
+            this.textBoxFind.TabIndex = 21;
+            this.textBoxFind.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listBoxTwo
+            // 
+            this.listBoxTwo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxTwo.FormattingEnabled = true;
+            this.listBoxTwo.ItemHeight = 15;
+            this.listBoxTwo.Location = new System.Drawing.Point(10, 521);
+            this.listBoxTwo.Name = "listBoxTwo";
+            this.listBoxTwo.Size = new System.Drawing.Size(508, 109);
+            this.listBoxTwo.TabIndex = 22;
+            // 
+            // buttonShowInfo
+            // 
+            this.buttonShowInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonShowInfo.Location = new System.Drawing.Point(10, 482);
+            this.buttonShowInfo.Name = "buttonShowInfo";
+            this.buttonShowInfo.Size = new System.Drawing.Size(148, 33);
+            this.buttonShowInfo.TabIndex = 23;
+            this.buttonShowInfo.Text = "See selected information";
+            this.buttonShowInfo.UseVisualStyleBackColor = true;
+            this.buttonShowInfo.Click += new System.EventHandler(this.buttonShowInfo_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label8.Font = new System.Drawing.Font("Verdana Pro Cond Black", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(0, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(189, 32);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Address Book";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(412, 472);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(108, 15);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "Search information";
+            // 
+            // FormOne
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1172, 633);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.buttonShowInfo);
+            this.Controls.Add(this.listBoxTwo);
+            this.Controls.Add(this.textBoxFind);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.listBoxOne);
+            this.Controls.Add(this.textBoxMail);
+            this.Controls.Add(this.textBoxNumber);
+            this.Controls.Add(this.textBoxLastname);
+            this.Controls.Add(this.textBoxAdress);
+            this.Controls.Add(this.textBoxPostNumber);
+            this.Controls.Add(this.textBoxCity);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormOne";
+            this.Text = "Adress Book";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxCity;
+        private System.Windows.Forms.TextBox textBoxPostNumber;
+        private System.Windows.Forms.TextBox textBoxAdress;
+        private System.Windows.Forms.TextBox textBoxLastname;
+        private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.TextBox textBoxMail;
+        private System.Windows.Forms.ListBox listBoxOne;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.TextBox textBoxFind;
+        private System.Windows.Forms.ListBox listBoxTwo;
+        private System.Windows.Forms.Button buttonShowInfo;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ AdressBook-Inlämningsuppgift 1/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace AdressBook_Inlämningsuppgift_1
+{
+    public partial class FormOne : Form
+    {
+
+        string myPath = @"C:\Users\46704\Desktop\AdressBook\AdressInformation.txt";
+
+        public FormOne()
+        {
+
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+
+            using (StreamWriter sw = new StreamWriter(myPath, true))
+            {
+                sw.WriteLine(textBoxName.Text + ", " + textBoxLastname.Text + ", " + textBoxAdress.Text + ", " + textBoxPostNumber.Text + ", " +
+                textBoxCity.Text + ", " + textBoxNumber.Text + ", " + textBoxMail.Text);
+
+                
+                MessageBox.Show("Information added!");
+                
+            }
+            
+
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+
+            string toString = listBoxOne.SelectedItem.ToString();           
+
+            var update = File.ReadAllText(myPath);
+            File.WriteAllText(myPath, update.Replace(toString, textBoxName.Text + ", " + textBoxLastname.Text + ", " + textBoxAdress.Text + ", " + textBoxPostNumber.Text + ", " +
+            textBoxCity.Text + ", " + textBoxNumber.Text + ", " + textBoxMail.Text));
+
+            
+            listBoxOne.Items.Remove(listBoxOne.SelectedItem);
+            listBoxOne.Items.Add(textBoxName.Text + ", " + textBoxLastname.Text + ", " + textBoxAdress.Text + ", " + textBoxPostNumber.Text + ", " +
+            textBoxCity.Text + ", " + textBoxNumber.Text + ", " + textBoxMail.Text);
+            
+            
+
+            MessageBox.Show("Updated sucess!");
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            listBoxOne.Items.Clear();
+
+            List<string> lines = File.ReadAllLines(myPath).ToList();
+
+            foreach (string line in lines)
+            {
+                listBoxOne.Items.Add(line);
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+             
+                       
+            string change = listBoxOne.SelectedItem.ToString();
+            string delete = File.ReadAllText(myPath);           
+            File.WriteAllText(myPath, delete.Replace(change, null));
+
+
+            foreach (string s in listBoxOne.SelectedItems.OfType<string>().ToList())
+            listBoxOne.Items.Remove(s);
+
+            MessageBox.Show("Selected adresses deleted!");
+
+        }
+      
+            
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            listBoxTwo.Items.Clear();
+
+            string[] lines = File.ReadAllLines(myPath);
+            string toFind = textBoxFind.Text;
+
+            if (textBoxFind.Text == "") listBoxTwo.Items.Clear();
+
+            else
+            {
+                foreach (string s in lines)
+                {
+                    if (s.Contains(toFind)) listBoxTwo.Items.Add(s);
+                }
+            }
+            
+        }
+
+        private void buttonShowInfo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(listBoxTwo.SelectedItem.ToString());
+        }
+    }               
+}
